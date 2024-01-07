48d96ef428db64bd2ee240b84b934266f589befb
sunka <sunka@DESKTOP-23GDKFQ>
2024-01-08 01:53:20 +0530

    Initial Commit.

---
+++ shopping-cart/src/main/java/com/ecommerce/servlet/CancelOrderServlet.java
@@ -0,0 +1,41 @@
+package com.ecommerce.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import com.ecommerce.connection.DbCon;
+import com.ecommerce.dao.OrderDao;
+
+/**
+ * Servlet implementation class CancelOrderServlet
+ */
+@WebServlet("/cancel-order")
+public class CancelOrderServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try(PrintWriter out = response.getWriter()){
+			String id = request.getParameter("id");
+			if(id!=null) {
+				OrderDao orderDao = new OrderDao(DbCon.getConnection());
+				orderDao.cancelOrder(Integer.parseInt(id));
+			}
+			response.sendRedirect("orders.jsp");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+}
